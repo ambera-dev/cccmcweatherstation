ab5651e2964871e7656ebb81339a549510822a32
Andrew Pitts <apitts@caldwellschools.com>
2018-10-26 12:30:25 +0000

    Modified JSON load and HTML DOM manipulation to reside inside of an AJAX call.

--- js/general.js
+++ js/general.js
@@ -1,13 +1,20 @@
-let wData = JSON.parse(data);
 window.onload = function(){
-document.getElementById("temp").innerHTML = "Temp: " + wData.temp; 
-document.getElementById("highTemp").innerHTML = "High: "+wData.highTemp;
-document.getElementById("lowTemp").innerHTML = "Low: "+wData.lowTemp;
-document.getElementById("windSpeed").innerHTML = "Wind Speed: <br>"+wData.wind; 
-document.getElementById("rain").innerHTML = "Precipitation: <br>"+wData.rainToday; 
-document.getElementById("windChill").innerHTML = "Wind Chill: "+wData.windChill; 
-document.getElementById("dewPoint").innerHTML = "Dew Point: "+wData.dewPoint; 
-document.getElementById("baroPressure").innerHTML = "Pressure: "+wData.barometer; 
-document.getElementById("altitude").innerHTML = "Altitude: "+wData.altitude; 
-document.getElementById("lastRefresh").innerHTML = "Last Refresh: "+wData.timeUpdated; 
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let wData = JSON.parse(this.responseText);
+			document.getElementById("temp").innerHTML = "Temp: " + wData.temp; 
+			document.getElementById("highTemp").innerHTML = "High: " + wData.highTemp;
+			document.getElementById("lowTemp").innerHTML = "Low: " + wData.lowTemp;
+			document.getElementById("windSpeed").innerHTML = "Wind Speed: <br>" + wData.wind; 
+			document.getElementById("rain").innerHTML = "Precipitation: <br>" + wData.rainToday; 
+			document.getElementById("windChill").innerHTML = "Wind Chill: " + wData.windChill; 
+			document.getElementById("dewPoint").innerHTML = "Dew Point: " + wData.dewPoint; 
+			document.getElementById("baroPressure").innerHTML = "Pressure: " + wData.barometer; 
+			document.getElementById("altitude").innerHTML = "Altitude: " + wData.altitude; 
+			document.getElementById("lastRefresh").innerHTML = "Last Refresh: " + wData.timeUpdated; 
+			}
+		}
+		xmlhttp.open("GET", "data.json", true);
+		xmlhttp.send();
 };
