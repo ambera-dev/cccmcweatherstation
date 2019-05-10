4a210a97a3542ce0f8cea897640df801c76747d2
aatkinscccmc <ambera1009@outlook.com>
2019-05-09 21:15:30 -0400

    Major changes. Bootstrap card layout adapted, background color and header color matched, footer added with useful links. I will add an issue request with some changes that need to be made before this is put on the server.

--- js/general.js
+++ js/general.js
@@ -3,14 +3,14 @@ window.onload = function(){
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			let wData = JSON.parse(this.responseText);
-			document.getElementById("temp").innerHTML = "Temp: " + wData.temp; 
+			document.getElementById("temp").innerHTML = "Current Temperature: " + wData.temp; 
 			document.getElementById("highTemp").innerHTML = "High: " + wData.highTemp;
 			document.getElementById("lowTemp").innerHTML = "Low: " + wData.lowTemp;
 			document.getElementById("windSpeed").innerHTML = "Wind Speed: <br>" + wData.wind; 
 			document.getElementById("rain").innerHTML = "Precipitation: <br>" + wData.rainToday; 
-			document.getElementById("windChill").innerHTML = "Wind Chill: " + wData.windChill; 
-			document.getElementById("dewPoint").innerHTML = "Dew Point: " + wData.dewPoint; 
-			document.getElementById("baroPressure").innerHTML = "Pressure: " + wData.barometer; 
+			document.getElementById("windChill").innerHTML = "Wind Chill: <br>" + wData.windChill; 
+			document.getElementById("dewPoint").innerHTML = "Dew Point: <br>" + wData.dewPoint; 
+			document.getElementById("baroPressure").innerHTML = "Pressure: <br>" + wData.barometer; 
 			document.getElementById("altitude").innerHTML = "Altitude: " + wData.altitude; 
 			document.getElementById("lastRefresh").innerHTML = "Last Refresh: " + wData.timeUpdated; 
 			}
